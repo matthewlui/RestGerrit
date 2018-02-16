1fd81e64479cfc4614383bc53a89e9c1300b3b16
Matthew Lui <matthewluihk@gmail.com>
2018-02-16 14:52:11 +0800

    Initial Commit

---
+++ RestGerrit/RestGerrit.h
@@ -0,0 +1,19 @@
+//
+//  RestGerrit.h
+//  RestGerrit
+//
+//  Created by Matthew Lui on 16/2/2018.
+//  Copyright © 2018 Chatboy.xyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RestGerrit.
+FOUNDATION_EXPORT double RestGerritVersionNumber;
+
+//! Project version string for RestGerrit.
+FOUNDATION_EXPORT const unsigned char RestGerritVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestGerrit/PublicHeader.h>
+
+
